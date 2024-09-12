7c1cde93a229fc2cf80558981ee7176fdb3e788e
TRPeck <13640563+TRPeck@users.noreply.github.com>
2024-09-12 11:21:42 -0700

    Add functionality to clear and equals buttons
    Clear button now clears display and resets variables. Equals button
    displays result. Fixed bug where numbers would add to calculated
    answer rather than overwriting it.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let firstNum = 0;
-let secondNum = 0;
-let operator = "";
-
 function add(a,b) {
     return a + b;
 }
@@ -32,16 +28,71 @@ function operate(op, a, b) {
 }
 
 function initializeButtons() {
+    let firstNum = null;
+    let secondNum = null;
+    let operator = "";
     const nums = document.querySelectorAll(".btn.num");
     let dispValue = document.querySelector(".answer");
+    let lastOperator = false;
+    //let dispExpression = document.querySelector(".expression");
     nums.forEach((btn) => {
         btn.addEventListener("click", () => {
             const val = btn.textContent;
-            if(dispValue.textContent != "0") {
+            if (dispValue.textContent != "" && lastOperator == true) {
+                dispValue.textContent = "";
+                dispValue.textContent = val;
+            }
+            else if(dispValue.textContent != "0") {
                 dispValue.textContent += val;
             }
             else {
                 dispValue.textContent = val;
+            }
+            lastOperator = false;
+        });
+    });
+
+    const clearBtn = document.querySelector(".btn.clear");
+    clearBtn.addEventListener("click", () => {
+        dispValue.textContent = "";
+        firstNum = null;
+        secondNum = null;
+        operator = "";
+    });
+
+    const equalsBtn = document.querySelector(".btn.equals");
+    equalsBtn.addEventListener("click", () => {
+        if(lastOperator == true) {
+            dispValue.textContent = firstNum;
+        }
+        else {
+            secondNum = parseFloat(dispValue.textContent);
+            dispValue.textContent = operate(operator, firstNum, secondNum);
+            firstNum = parseFloat(dispValue.textContent);
+        }
+        firstNum = null;
+        secondNum = null;
+        lastOperator = false;
+        operator = "";
+    });
+
+    const operations = document.querySelectorAll(".btn.op");
+    operations.forEach(btn => {
+        const op = btn.textContent;
+        btn.addEventListener("click", () => {
+            if(firstNum === null && dispValue.textContent !== "") {
+                //dispExpression.textContent = dispValue.textContent + op;
+                firstNum = parseFloat(dispValue.textContent);
+                operator = op;
+                lastOperator = true;
+            }
+            else if(firstNum !== null) {
+                secondNum = parseFloat(dispValue.textContent);
+                dispValue.textContent = operate(operator, firstNum, secondNum);
+                firstNum = parseFloat(dispValue.textContent);
+                operator = op;
+                lastOperator = true;
+                //dispExpression.textContent = dispValue.textContent + op;
             }
         });
     });
